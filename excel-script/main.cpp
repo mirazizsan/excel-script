f5ddaf4bd2ea7e5fd4113553f37a6f3b1b5f3a97
mirazizsan <67619157+mirazizsan@users.noreply.github.com>
2020-07-27 02:10:19 +0300

    Initial commit

---
+++ excel-script/main.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <fstream> //library for work w/ files
+
+using namespace std;
+
+int main()
+{
+	string path = "myFile.csv";
+	ofstream fout; //output
+
+	fout.open(path);
+
+	/*if (!fout.is_open())
+	{
+		cout << "Error: File is not opened!" << endl;
+	}
+	else
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			fout << i << ";" << i * i << endl;
+		}
+	}*/
+
+	try
+	{
+		if (!fout.is_open())
+			throw std::invalid_argument
+			("ERROR: File did not open!");
+		{
+			for (int i = 1; i <= 20; i++)
+			{
+				fout << i << ";" << i * i << endl;
+			}
+		}
+	}
+	catch (const std::invalid_argument& e)
+	{
+		cout << e.what() << endl;
+	}
+	fout.close();
+
+	system("pause");
+	return 0;
+}
